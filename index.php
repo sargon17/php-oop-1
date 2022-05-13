f38a0c7ce994a83385452eb21b5c32fc43ee1057
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-13 19:27:15 +0200

    created html block

--- index.php
+++ index.php
@@ -6,8 +6,21 @@ $myMovie = new Movie(
   "The Matrix",
   1999,
   "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg",
-  [5, 5, 5, 5, 5],
+  [5, 5, 5, 3, 5],
   1
 );
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $myMovie->getRating(); ?>
+    
+</body>
+</html>
