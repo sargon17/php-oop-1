0ec8c89ead64ae9428c33c56500dc4873de3d1ed
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-13 19:22:39 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include "Movie.php";
+
+$myMovie = new Movie(
+  "The Matrix",
+  1999,
+  "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg",
+  [5, 5, 5, 5, 5],
+  1
+);
+
+?>
